c2ff91a9f15ea9766fb930724667d0b44af58d19
dm22dh <dm22dh@brocku.ca>
2023-12-18 20:37:20 -0500

    changes

---
+++ Assignment04/script.js
@@ -0,0 +1,5 @@
+function showName(e){
+    var firstName= document.getElementById("firstname").value;
+    // var lastName = document.getElementById("lastname").value;
+    document.getElementById("show-name").innerText = "Welcome" + " " + firstName+" " + "!";        
+}
